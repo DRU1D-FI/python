3b785dc07609d709d450170dbe31c824ac7fe20a
Sam Kaarlas <sam.kaarlas@gmail.com>
2025-03-23 16:33:13 +0100

    added way to add and view tasks

--- Planer/Planer.py
+++ Planer/Planer.py
@@ -3,3 +3,26 @@ class Event:
         self.date = date
         self.description = description
         self.type = type
+        self.id = 1
+
+class Tasklist:
+    def __init__(self):
+        self.tasklist = []
+        return
+
+    def gettasks(self):
+        return self.tasklist
+
+
+    def addtask(self, task: Event): 
+        self.tasklist.append(task)
+        return
+    
+    def delete(self, id):
+        return
+
+
+class Saver:
+    def save(self, message, user_list):
+        user_input = input(message)
+        user_list.append(user_input)
--- Planer/PlanerApp.py
+++ Planer/PlanerApp.py
@@ -1,23 +1,39 @@
-from Planer import Event
+from Planer import Event, Tasklist
 
 def main1():
     number = 1
     print("python main function")
     print(number)
     task1 = Event("20.03.25" , "Math page 15" , "Homework")
-    date = input("Enter the date: ")
-    description = input("Enter the description:")
-    type = input("enter the type: ")
-    print(date , description , type)
-    print("===========")
-    task = Event(date , description , type)
-    tasklist = []
-    tasklist.append(task)
-    tasklist.append(task1)
+    task2 = Event("23.03.25" , "German page 3" , "homework")
+    mylist = Tasklist()
+    mylist.addtask(task1)
+    mylist.addtask(task2)
+    # date = input("Enter the date: ")
+    # description = input("Enter the description:")
+    # type = input("enter the type: ")
+    # print(date , description , type)
+    # print("===========")
+    # task = Event(date , description , type)
+    # tasklist = []
+    # tasklist.append(task)
+    # tasklist.append(task1)
+    tasks = mylist.gettasks()
     
-    for task in tasklist:
-        print(task.date , task.description , task.type)
+    
+    for task in tasks:
+        print(task.description , task.type , task. date)
 
 
 if __name__ == '__main__':
     main1()
+
+# from Planer import Saver
+
+# my_list = []
+# saver = Saver()
+
+# saver.save("Enter something: ", my_list)
+# saver.save("Enter another thing: ", my_list)
+
+# print("Your list:", my_list)
